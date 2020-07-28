1c6290fe6b6fb594ef113f7ba26b33c90908c9b1
barbaraaliverti <barbaraaliverti@gmail.com>
2020-07-28 18:17:30 -0300

    Segunda aula da imersão

--- src/index.js
+++ src/index.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './Pages/Home';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
+import CadastroVideo from '../src/Pages/cadastro/video';
+import CadastroCategoria from '../src/Pages/cadastro/categoria';
 
+
+const Pagina404 = () => (<div>Página 404</div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="/" component={Home} exact/>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={Pagina404} />
+    </Switch>    
+  </BrowserRouter>,
   document.getElementById('root')
 );
 
